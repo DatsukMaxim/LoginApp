72352c6bf5548cb9010c29ae5a48a9e78147e9b0
Datsuk Maxim <datsuk.job@gmail.com>
2022-07-04 02:35:52 +0700

    Added new view with gradient

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Maxim Datsuk on 04.07.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+class GradientView: UIView {
+    
+    @IBInspectable private var topColor: UIColor? {
+        didSet {
+            setupGradientColors()
+        }
+    }
+    
+    @IBInspectable private var bottomColor: UIColor? {
+        didSet {
+            setupGradientColors()
+        }
+    }
+
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    private func setupGradient() {
+        self.layer.addSublayer(gradientLayer)
+        
+        setupGradientColors()
+    }
+    
+    private func setupGradientColors() {
+        if let topColor = topColor, let bottomColor = bottomColor {
+            gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        }
+    }
+}
