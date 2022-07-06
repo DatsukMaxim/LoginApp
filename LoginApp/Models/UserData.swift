3a6c7483ff9be66552d61a093fc7831801b15068
Datsuk Maxim <datsuk.job@gmail.com>
2022-07-07 04:57:11 +0700

    Edit text in label

---
+++ LoginApp/Models/UserData.swift
@@ -0,0 +1,28 @@
+//
+//  UserData.swift
+//  LoginApp
+//
+//  Created by Maxim Datsuk on 07.07.2022.
+//
+
+import Foundation
+
+struct User {
+    let login = "Max"
+    let password = "1234"
+    let person = ""
+}
+
+struct Person {
+    let name = "Максим"
+    let surname = "Дацук"
+    let age = "24"
+    let photo = "me.jpg"
+    let work = "С 2019 г. работаю на должности Automation QA Engineer в компании \"Софт Инжиниринг\", г. Кемерово"
+    
+    let hobby = "Свободного времени не так много, но если удается его найти, то встречаюсь с друзьями"
+    let hobbyPhoto = "friends.jpg"
+    
+    let pet = "А еще у меня есть вот такой котик, его зовут Тимофей :3"
+    let petImage = "cat.jpg"
+}
